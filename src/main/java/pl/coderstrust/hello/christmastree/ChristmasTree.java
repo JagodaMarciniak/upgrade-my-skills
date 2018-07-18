fca45840741508188bfcd64cf07e9c19a8c71613
Jagoda <jagodaciniak@gmail.com>
2018-07-18 23:43:49 +0200

    rename method, rename argument

--- src/main/java/pl/coderstrust/hello/christmastree/ChristmasTree.java
+++ src/main/java/pl/coderstrust/hello/christmastree/ChristmasTree.java
@@ -2,13 +2,12 @@ package pl.coderstrust.hello.christmastree;
 
 public class ChristmasTree {
     public static void main(String[] args) {
-        treeSize(6);
+        printTree(6);
     }
 
-    public static void treeSize(int a) {
-
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < a - i; j++) {
+    public static void printTree(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height - i; j++) {
                 System.out.print(" ");
             }
             for (int k = 0; k < (2 * i + 1); k++) {
@@ -16,10 +15,10 @@ public class ChristmasTree {
             }
             System.out.println();
         }
-        for (int i = 0; i < a - 1; i++) {
+        for (int i = 0; i < height - 1; i++) {
             System.out.print(" ");
         }
-        for (int i = a - 2; i < a; i++) {
+        for (int i = height - 2; i < height; i++) {
             System.out.print("*");
         }
     }
