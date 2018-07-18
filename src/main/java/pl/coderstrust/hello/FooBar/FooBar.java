6bd4d972d91c89a06d1ba3317636e428f952f7e4
Jagoda <jagodaciniak@gmail.com>
2018-07-18 23:38:21 +0200

    line corrected

--- src/main/java/pl/coderstrust/hello/FooBar/FooBar.java
+++ src/main/java/pl/coderstrust/hello/FooBar/FooBar.java
@@ -3,7 +3,6 @@ package pl.coderstrust.hello.FooBar;
 public class FooBar {
 
     public static void main(String[] args) {
-
         for (int i = 0; i <= 100; i++) {
             if (i == 0) {
                 System.out.println(i);
