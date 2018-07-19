e0dd486c4191f185da9012dd8134631cf8e7ff80
Jagoda <jagodaciniak@gmail.com>
2018-07-19 12:58:51 +0200

    printf in proces, I have no Idea..

--- src/main/java/pl/coderstrust/multiplicationTable/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplicationTable/MultiplicationTable.java
@@ -2,27 +2,27 @@ package pl.coderstrust.multiplicationTable;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        multiplicationTableSize(12);
+        printTable(12);
     }
 
-    public static void multiplicationTableSize(int a) {
+    public static void printTable(int size) {
         System.out.print("  ");
-        for (int j = 1; j <= a; j++) {
+        for (int j = 1; j <= size; j++) {
             if (j < 10) {
-                System.out.print("   " + j);
+                System.out.printf("   %d", j);
             }
             if (j >= 10) {
-                System.out.print("  " + j);
+                System.out.printf("  %d\n", j);
             }
         }
         System.out.println();
-        for (int i = 1; i <= a; i++) {
+        for (int i = 1; i <= size; i++) {
             if (i > 9) {
                 System.out.print(i);
             } else {
-                System.out.print(" " + i);
+                System.out.printf(" %d\n", i);
             }
-            for (int j = 1; j <= a; j++) {
+            for (int j = 1; j <= size; j++) {
                 if (i * j < 10) {
                     System.out.print("   " + i * j);
                 }
