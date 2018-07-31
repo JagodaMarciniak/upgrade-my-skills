4a210b6cd58b72ee03e899627b10c2372155d1fc
Jagoda <jagodaciniak@gmail.com>
2018-07-31 12:51:50 +0200

    erasing the code. To review, again.

--- src/main/java/pl/coderstrust/FooBar/FooBar.java
+++ src/main/java/pl/coderstrust/FooBar/FooBar.java
@@ -3,17 +3,14 @@ package pl.coderstrust.FooBar;
 public class FooBar {
     public static void main(String[] args) {
         for (int i = 0; i <= 100; i++) {
-            if (i == 0) {
-                System.out.println(i);
-            } else if (i % 5 == 0 && i % 3 == 0) {
-                System.out.println(i + " FooBar");
-            } else if (i % 3 == 0) {
-                System.out.println(i + " Foo");
-            } else if (i % 5 == 0) {
-                System.out.println(i + " Bar");
-            } else {
-                System.out.println(i);
+            System.out.print(i);
+            if (i % 3 == 0) {
+                System.out.print(" Foo");
             }
+            if (i % 5 == 0) {
+                System.out.print(" Bar");
+            }
+            System.out.println();
         }
     }
 }
