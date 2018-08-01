cd7ba2cc4d8f9c587f43eb3ac6149aeb697c8d3d
Jagoda <jagodaciniak@gmail.com>
2018-08-01 15:12:57 +0200

    in progress

---
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -0,0 +1,22 @@
+package pl.coderstrust.sieve;
+
+public class SieveOfEratosthenes {
+    public static void main(String[] args) {
+        createTable(10);
+        printArray(array);
+    }
+
+    public static int [] createTable(int maximumNumber){
+        int table [] = new int[i];
+        for(int i=2;  i< maximumNumber; i++){
+
+        }
+        return table;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
