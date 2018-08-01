dcad72e5a37a194214733df2913512dcfe151639
Jagoda <jagodaciniak@gmail.com>
2018-08-01 22:37:56 +0200

    in progress

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -2,21 +2,31 @@ package pl.coderstrust.sieve;
 
 public class SieveOfEratosthenes {
     public static void main(String[] args) {
-        createTable(10);
-        printArray(array);
+//        printArray(createTable(20));
+        printArray(removeMultipliersOfTwo(createTable(20)));
     }
 
     public static int[] createTable(int maximumNumber) {
-        int table[] = new int[i];
-        for (int i = 2; i < maximumNumber; i++) {
-
+        int[] table = new int[maximumNumber - 1];
+        for (int i = 0; i < maximumNumber - 1; i++) {
+            table[i] = i + 2;
         }
         return table;
     }
 
-    public static void printArray(int[] array) {
-        for (int i : array) {
-            System.out.println(i);
+    public static int[] removeMultipliersOfTwo(int[] table) {
+        for (int i = 1; i < table.length - 1; i++) {
+            if (table[i] % 2 == 0) {
+                int shift = table[i + 1];
+                table[i] = shift;
+            }
+        }
+        return table;
+    }
+
+    public static void printArray(int[] table) {
+        for (int i : table) {
+            System.out.printf(" %d", i);
         }
     }
 }
