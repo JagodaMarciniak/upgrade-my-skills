91f2435143ef19c37232da9bf23e1c4793f922ae
Jagoda <jagodaciniak@gmail.com>
2018-08-02 18:04:02 +0200

    Change code like mentor suggested.

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -2,37 +2,22 @@ package pl.coderstrust.multiplication;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
+
         printMultiplicationTable(12);
     }
 
     public static void printMultiplicationTable(int size) {
         System.out.print("  ");
         for (int j = 1; j <= size; j++) {
-            if (j < 10) {
-                System.out.printf("%4d", j);
-            }
-            if (j >= 10) {
-                System.out.printf("  %d", j);
-            }
+            System.out.printf("%4d", j);
         }
         System.out.println();
         for (int i = 1; i <= size; i++) {
-            if (i > 9) {
-                System.out.print(i);
-            } else {
-                System.out.printf(" %d", i);
-            }
+            System.out.printf("%2d", i);
+
             for (int j = 1; j <= size; j++) {
                 int multi = i * j;
-                if (multi < 10) {
-                    System.out.printf("   %d", multi);
-                }
-                if (multi >= 10 && multi < 100) {
-                    System.out.printf("  %d", multi);
-                }
-                if (multi >= 100) {
-                    System.out.printf(" %d", multi);
-                }
+                System.out.printf("%4d", multi);
             }
             System.out.println();
         }
