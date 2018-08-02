60958e993938074c5df89091d934ff3f0285d7a3
Jagoda <jagodaciniak@gmail.com>
2018-08-02 17:37:38 +0200

    add file

--- src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
@@ -1,18 +1,18 @@
 package pl.coderstrust.fibonacci;
 
 public class FibonacciRecursive {
-        public static void main(String[] args) {
-            System.out.println(fibonacci(8));
-        }
+    public static void main(String[] args) {
+        System.out.println(fibonacci(8));
+    }
 
-        private static long fibonacci(int fibonacciNumberInOrder) {
-            if (fibonacciNumberInOrder == 0) {
-                return 0;
-            } else if (fibonacciNumberInOrder == 1) {
-                return 1;
-            } else {
-                return fibonacci(fibonacciNumberInOrder - 1)
-                        + fibonacci(fibonacciNumberInOrder - 2);
-            }
+    private static long fibonacci(int fibonacciNumberInOrder) {
+        if (fibonacciNumberInOrder == 0) {
+            return 0;
+        } else if (fibonacciNumberInOrder == 1) {
+            return 1;
+        } else {
+            return fibonacci(fibonacciNumberInOrder - 1)
+                    + fibonacci(fibonacciNumberInOrder - 2);
         }
     }
+}
