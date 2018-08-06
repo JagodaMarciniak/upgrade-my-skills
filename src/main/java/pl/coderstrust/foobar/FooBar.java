a5e70a700c66c5942552b66eb73c1570663b80dd
Jagoda <jagodaciniak@gmail.com>
2018-08-06 21:15:41 +0200

    FooBar test to review.

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -1,26 +1,31 @@
 package pl.coderstrust.foobar;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FooBar {
     public static void main(String[] args) {
-        fooOrBar(10);
+        System.out.println(getFooBar(1));
     }
 
-    private static List<String> fooOrBar(int maximumValue) {
+    public static List<String> getFooBar(int size) {
+        if (size < 0) {
+            return new ArrayList<String>(Collections.singletonList("Invalid value"));
+        }
         List<String> result = new ArrayList<String>();
-        for (int i = 0; i <= maximumValue; i++) {
-            System.out.print(i);
+        StringBuilder storage = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            storage.append(i + " ");
             if (i % 3 == 0) {
-                result.add("Foo");
-                System.out.print("Foo");
+                storage.append("Foo");
             }
             if (i % 5 == 0) {
-                result.add("Bar");
-                System.out.print("Bar");
+                storage.append("Bar");
             }
-            System.out.println();
+
+            result.add(storage.toString());
+            storage.delete(0, storage.length());
         }
         return result;
     }
