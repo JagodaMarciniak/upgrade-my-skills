c42774e95cb1a7a45593bcc905283c5835f3473e
Jagoda <jagodaciniak@gmail.com>
2018-08-11 15:59:08 +0200

    In progress

--- src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
@@ -10,22 +10,22 @@ public class ChristmasTree {
     }
 
     public static List<String> printChristmasTree(int height) {
-        if (height < 0) {
+        if (height <= 0) {
             return new ArrayList<String>(Collections.singletonList("Invalid value"));
         }
         List<String> result = new ArrayList<String>();
         StringBuilder storage = new StringBuilder();
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < height - i; j++) {
-                storage.append(" ");
+                storage.append(".");
             }
             for (int k = 0; k < (2 * i + 1); k++) {
                 storage.append("*");
             }
-            storage.append("\n ");
+            storage.append("\n.");
         }
         for (int i = 0; i < height - 1; i++) {
-            storage.append(" ");
+            storage.append(".");
         }
         storage.append("**");
         result.add(storage.toString());
--- src/test/java/pl/coderstrust/christmastree/ChristmasTreeTest.java
+++ src/test/java/pl/coderstrust/christmastree/ChristmasTreeTest.java
@@ -11,10 +11,33 @@ import static org.junit.Assert.assertTrue;
 public class ChristmasTreeTest {
 
     @Test
-    public void shouldTestprintChristmasTreeWithZeroValue() {
+    public void shouldTestZeroValue() {
+//        when
+        int size = 0;
+        List<String> expected = new ArrayList<String>(Collections.singletonList("Invalid value"));
+//        given
+        List<String> actual = new ArrayList<String>(ChristmasTree.printChristmasTree(size));
+//        then
+        assertTrue(expected.equals(actual));
+    }
+
+    public void shouldTestNegativeValues() {
 //        when
         int size = -5;
         List<String> expected = new ArrayList<String>(Collections.singletonList("Invalid value"));
+//        given
+        List<String> actual = new ArrayList<String>(ChristmasTree.printChristmasTree(size));
+//        then
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void shouldTestPositiveValues() {
+//        when
+        int size = 5;
+        List<String> expected = new ArrayList<String>();
+        expected.add(".....*\n.....***\n....*****\n...*******\n..*********\n.....**");
+
 //        given
         List<String> actual = new ArrayList<String>(ChristmasTree.printChristmasTree(size));
 //        then
