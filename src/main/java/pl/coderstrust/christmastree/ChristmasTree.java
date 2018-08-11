ce38cd89f3bac1261792746b92e554af5287bcc2
Jagoda <jagodaciniak@gmail.com>
2018-08-11 13:57:49 +0200

    In progress

--- src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
@@ -1,14 +1,18 @@
 package pl.coderstrust.christmastree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChristmasTree {
     public static void main(String[] args) {
         printChristmasTree(6);
     }
 
     public static void printChristmasTree(int height) {
+        List<String> result = new ArrayList<String>();
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < height - i; j++) {
-                System.out.print(" ");
+              storage.append(" ");
             }
             for (int k = 0; k < (2 * i + 1); k++) {
                 System.out.print("*");
