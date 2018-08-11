5e140c5aa40954b98b0c2595364863e6fccebaa4
Jagoda <jagodaciniak@gmail.com>
2018-08-11 17:44:35 +0200

    In progress

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -2,14 +2,16 @@ package pl.coderstrust.multiplication;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        printMultiplicationTable(5);
+        System.out.println(printMultiplicationTable(5));
     }
 
-    public static ArrayList printMultiplicationTable(int size) {
+    public static List<String> printMultiplicationTable(int size) {
         ArrayList<String> result = new ArrayList<String>();
+        StringBuilder storage = new StringBuilder();
         if (size <= 0) {
             return new ArrayList<String>(Collections.singleton("Invalid value"));
         }
@@ -17,9 +19,12 @@ public class MultiplicationTable {
         for (int row = 0; row < multiplicationTable.length; row++) {
             for (int col = 0; col < multiplicationTable[row].length; col++) {
                 multiplicationTable[row][col] = (row + 1) * (col + 1);
+                int number = multiplicationTable[row][col];
+                storage.append("  " + number);
             }
+            storage.append("\n");
         }
-
+        result.add(storage.toString());
         return result;
     }
 }
--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -13,12 +13,11 @@ public class MultiplicationTableTest {
     public void shouldTestZeroValue() {
         // when
         int size = 0;
-        List<String> expected = new ArrayList<String>();
-        expected.add("Invalid value");
+        List<String> expected = new ArrayList<String>(Collections.singleton("Invalid value"));
 
 //         given
-        ArrayList<String> actual = new ArrayList<String>();
-        actual = MultiplicationTable.printMultiplicationTable(size);
+        List<String> actual = new ArrayList<String>(MultiplicationTable.printMultiplicationTable(size));
+
 
         // then
         assertTrue(expected.equals(actual));
@@ -27,11 +26,12 @@ public class MultiplicationTableTest {
     @Test
     public void shouldTestNegativeValues() {
         // when
-        int size = -7;
-        ArrayList<String> expected = new ArrayList<String>(Collections.singleton("Invalid value"));
+        int size = -8;
+        List<String> expected = new ArrayList<String>(Collections.singleton("Invalid value"));
 
-        // given
-        ArrayList<String> actual = MultiplicationTable.printMultiplicationTable(size);
+//         given
+        List<String> actual = new ArrayList<String>(MultiplicationTable.printMultiplicationTable(size));
+
 
         // then
         assertTrue(expected.equals(actual));
@@ -41,34 +41,13 @@ public class MultiplicationTableTest {
     public void shouldTestPositiveValues() {
         // when
         int size = 10;
-        ArrayList<String> expected = new {
-            {
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-            },{
-                2, 4, 6, 8, 10, 12, 14, 16, 18, 20
-            },{
-                3, 6, 9, 12, 15, 18, 21, 24, 27, 30
-            },{
-                4, 8, 12, 16, 20, 24, 28, 32, 36, 40
-            },{
-                5, 10, 15, 20, 25, 30, 35, 40, 45, 50
-            },{
-                6, 12, 18, 24, 30, 36, 42, 48, 54, 60
-            },{
-                7, 14, 21, 28, 35, 42, 49, 56, 63, 70
-            },{
-                8, 16, 24, 32, 40, 48, 56, 64, 72, 80
-            },{
-                9, 18, 27, 36, 45, 54, 63, 72, 81, 90
-            },{
-                10, 20, 30, 40, 50, 60, 70, 80, 90, 100
-            }
-        } ;
+        List<String> expected = new ArrayList<String>();
+
 
         // given
-        int[][] actual = MultiplicationTable.printMultiplicationTable(size);
+        List<String> actual = new ArrayList<String>(MultiplicationTable.printMultiplicationTable(size));
 
         // then
-        assertArrayEquals(expected, actual);
+        assertTrue(expected.equals(actual));
     }
 }
