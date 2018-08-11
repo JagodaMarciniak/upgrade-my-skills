aa0e64361480b37b7184d42092a65d9a4704c58a
Jagoda <jagodaciniak@gmail.com>
2018-08-11 15:29:35 +0200

    In progress

--- src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
@@ -1,14 +1,18 @@
 package pl.coderstrust.christmastree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ChristmasTree {
     public static void main(String[] args) {
-        System.out.println(printChristmasTree(0));
+        System.out.println(printChristmasTree(5));
     }
 
     public static List<String> printChristmasTree(int height) {
+        if (height < 0) {
+            return new ArrayList<String>(Collections.singletonList("Invalid value"));
+        }
         List<String> result = new ArrayList<String>();
         StringBuilder storage = new StringBuilder();
         for (int i = 0; i < height; i++) {
--- src/test/java/pl/coderstrust/christmastree/ChristmasTreeTest.java
+++ src/test/java/pl/coderstrust/christmastree/ChristmasTreeTest.java
@@ -2,12 +2,22 @@ package pl.coderstrust.christmastree;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class ChristmasTreeTest {
 
     @Test
     public void shouldTestprintChristmasTreeWithZeroValue() {
 //        when
-        int size = 0;
-
+        int size = -5;
+        List<String> expected = new ArrayList<String>(Collections.singletonList("Invalid value"));
+//        given
+        List<String> actual = new ArrayList<String>(ChristmasTree.printChristmasTree(size));
+//        then
+        assertTrue(expected.equals(actual));
     }
 }
