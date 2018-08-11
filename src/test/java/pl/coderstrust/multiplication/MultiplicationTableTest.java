e316fa3fb43f3251a9270b3fbae37dd64a732550
Jagoda <jagodaciniak@gmail.com>
2018-08-11 13:11:50 +0200

    add an test file

--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -1,14 +1,12 @@
 package pl.coderstrust.multiplication;
 
-import static org.junit.Assert.*;
-
 public class MultiplicationTableTest {
 
     @org.junit.Test
     public void shouldCreateMultiplicationTableWithSize10() {
         // when
         int size = 10;
-     // given
-        int [] [] testTable = 
+        // given
+        int[][] testTable =
     }
 }
