8574a0e6e70309f63c8bb1cd63c4df5f3423f881
Jagoda <jagodaciniak@gmail.com>
2018-08-11 13:11:04 +0200

    In process

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -1,25 +1,29 @@
 package pl.coderstrust.multiplication;
 
+import java.util.Arrays;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
 
-        printMultiplicationTable(12);
-    }
-
-    public static void printMultiplicationTable(int size) {
-        System.out.print("  ");
-        for (int j = 1; j <= size; j++) {
-            System.out.printf("%4d", j);
-        }
+        int[][] myTable = createMultiplicationTable(10);
+        System.out.print(Arrays.deepToString(myTable));
         System.out.println();
-        for (int i = 1; i <= size; i++) {
-            System.out.printf("%2d", i);
-
-            for (int j = 1; j <= size; j++) {
-                int multi = i * j;
-                System.out.printf("%4d", multi);
+        for (int row = 0; row < myTable.length; row++) {
+            for (int column = 0; column < myTable[row].length; column++) {
+                System.out.printf("%2d ", myTable[row][column]);
             }
             System.out.println();
         }
+    }
+
+    public static int[][] createMultiplicationTable(int size) {
+        int[][] multiplicationTable = new int[size][size];
+        System.out.println();
+        for (int row = 0; row < multiplicationTable.length; row++) {
+            for (int col = 0; col < multiplicationTable[row].length; col++) {
+                multiplicationTable[row][col] = (row + 1) * (col + 1);
+            }
+        }
+        return multiplicationTable;
     }
 }
---
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -0,0 +1,14 @@
+package pl.coderstrust.multiplication;
+
+import static org.junit.Assert.*;
+
+public class MultiplicationTableTest {
+
+    @org.junit.Test
+    public void shouldCreateMultiplicationTableWithSize10() {
+        // when
+        int size = 10;
+     // given
+        int [] [] testTable = 
+    }
+}
