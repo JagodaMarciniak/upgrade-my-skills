1b778ffcc27277a7be93c0c4f6ade25893ec5593
Jagoda <jagodaciniak@gmail.com>
2018-08-13 23:54:13 +0200

    to review

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -1,32 +1,24 @@
 package pl.coderstrust.multiplication;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        System.out.println(getMultiplicationTable(5));
+        int[][] table = getMultiplicationTable(5);
+        System.out.println(Arrays.deepToString(table));
     }
 
-    public static List<String> getMultiplicationTable(int size) {
-        ArrayList<String> result = new ArrayList<String>();
-        StringBuilder storage = new StringBuilder();
+    public static int[][] getMultiplicationTable(int size) {
         if (size <= 0) {
-            return new ArrayList<String>(Collections.singleton("Invalid value"));
+            return new int[0][0];
         }
         int[][] multiplicationTable = new int[size][size];
         for (int row = 0; row < multiplicationTable.length; row++) {
             for (int col = 0; col < multiplicationTable[row].length; col++) {
                 multiplicationTable[row][col] = (row + 1) * (col + 1);
                 int number = multiplicationTable[row][col];
-                storage.append(number + " ");
             }
-            result.add(storage.toString());
-            storage.delete(0, storage.length());
         }
-        result.add(storage.toString());
-        storage.delete(0, storage.length());
-        return result;
+        return multiplicationTable;
     }
 }
--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -2,54 +2,50 @@ package pl.coderstrust.multiplication;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 public class MultiplicationTableTest {
     @Test
     public void testZeroSize() {
         // given
         int size = 0;
-        List<String> expected = new ArrayList<String>(Collections.singleton("Invalid value"));
+        int[][] expected = new int[0][0];
 
         // when
-        List<String> actual = new ArrayList<String>(MultiplicationTable.getMultiplicationTable(size));
+        int[][] actual = MultiplicationTable.getMultiplicationTable(size);
 
         // then
-        assertTrue(expected.equals(actual));
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testNegativeSize() {
         // given
         int size = -8;
-        List<String> expected = new ArrayList<String>(Collections.singleton("Invalid value"));
+        int[][] expected = new int[0][0];
 
         // when
-        List<String> actual = new ArrayList<String>(MultiplicationTable.getMultiplicationTable(size));
+        int[][] actual = MultiplicationTable.getMultiplicationTable(size);
 
         // then
-        assertTrue(expected.equals(actual));
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testSizeOfFive() {
         // given
         int size = 5;
-        List<String> expected = new ArrayList<String>();
-        expected.add("1 2 3 4 5 ");
-        expected.add("2 4 6 8 10 ");
-        expected.add("3 6 9 12 15 ");
-        expected.add("4 8 12 16 20 ");
-        expected.add("5 10 15 20 25 ");
+        int[][] expected = new int[][]{
+                {1, 2, 3, 4, 5},
+                {2, 4, 6, 8, 10},
+                {3, 6, 9, 12, 15},
+                {4, 8, 12, 16, 20},
+                {5, 10, 15, 20, 25}};
 
         // when
-        List<String> actual = new ArrayList<String>(MultiplicationTable.getMultiplicationTable(size));
+        int[][] actual = MultiplicationTable.getMultiplicationTable(size);
 
         // then
-        assertTrue(expected.equals(actual));
+        assertArrayEquals(expected, actual);
     }
 }
