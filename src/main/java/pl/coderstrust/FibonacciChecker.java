ed9a7b88df015190a534eca027e4462162aabd37
Jagoda <jagodaciniak@gmail.com>
2018-08-15 00:04:41 +0200

    Is fibonacci in progress

--- src/main/java/pl/coderstrust/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/FibonacciChecker.java
@@ -3,21 +3,21 @@ package pl.coderstrust;
 public class FibonacciChecker {
 
     public static void main(String[] args) {
-        isFibonacciNumber(377);
+        System.out.println(isFibonacciNumber(21));
     }
 
     public static boolean isFibonacciNumber(long number) {
+        if (number < 2) {
+            return false;
+        }
         long recognizingPerfectSquareFormula1 = number * number * 5 + 4;
         long recognizingPerfectSquareFormula2 = number * number * 5 - 4;
         double perfectSquareVariant1 = Math.sqrt(recognizingPerfectSquareFormula1);
         double perfectSquareVariant2 = Math.sqrt(recognizingPerfectSquareFormula2);
         if ((int) perfectSquareVariant1 * (int) perfectSquareVariant1 == recognizingPerfectSquareFormula1
                 || (int) perfectSquareVariant2 * (int) perfectSquareVariant2 == recognizingPerfectSquareFormula2) {
-            System.out.println("true");
             return true;
         }
-        System.out.println("false");
         return false;
     }
-
 }
--- src/test/java/pl/coderstrust/FibonacciCheckerTest.java
+++ src/test/java/pl/coderstrust/FibonacciCheckerTest.java
@@ -1,6 +1,56 @@
 package pl.coderstrust;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class FibonacciCheckerTest {
+    @Test
+    public void testNumberOfZero() {
+        //given
+        int number = 0;
 
+        //when
+        boolean expected = FibonacciChecker.isFibonacciNumber(number);
 
+        //then
+        assertFalse(expected);
+    }
+
+    @Test
+    public void testNegativeNumber() {
+        //given
+        int number = -10;
+
+        //when
+        boolean expected = FibonacciChecker.isFibonacciNumber(number);
+
+        //then
+        assertFalse(expected);
+    }
+
+    @Test
+    public void testNumberOfTwentyOne() {
+        //given
+        int number = 21;
+
+        //when
+        boolean expected = FibonacciChecker.isFibonacciNumber(number);
+
+        //then
+        assertTrue(expected);
+    }
+
+    @Test
+    public void testNumberOfTwoHundredAndThirtyThree() {
+        //given
+        long number = 233;
+
+        //when
+        boolean expected = FibonacciChecker.isFibonacciNumber(number);
+
+        //then
+        assertTrue(expected);
+    }
 }
