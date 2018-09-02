29c2c99067f4637f03f397297d1fd94acc5fdb5d
Jagoda <jagodaciniak@gmail.com>
2018-09-02 20:39:40 +0200

    add Merge sort

--- src/test/java/pl/coderstrust/timeCompareSorting/SelectionSortMethodTest.java
+++
@@ -1,9 +0,0 @@
-package pl.coderstrust.timeCompareSorting;
-
-//public class SelectionSortMethodTest extends SortingTestBase {
-//
-//    @Override
-//    public SortingMethod getSortingMethod() {
-//        return new SelectionSortMethod();
-//    }
-//}
