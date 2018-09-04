0d8485e31e253f37f492f17e4e2c820a2af8e8e3
Jagoda <jagodaciniak@gmail.com>
2018-09-04 20:42:10 +0200

    .

--- src/main/java/pl/coderstrust/numbersProcessor/NumbersProcessor.java
+++ src/main/java/pl/coderstrust/numbersProcessor/NumbersProcessor.java
@@ -1,5 +1,30 @@
 package pl.coderstrust.numbersProcessor;
 
-public class NumbersProcessor {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class NumbersProcessor {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("23455" +
+                "232");
+        processLine(list);
+    }
+
+    public static void processLine(List<String> list) {
+        StringBuilder storage = new StringBuilder();
+        Scanner scanner = new Scanner(String.valueOf(list));
+        while (scanner.hasNextLine()) {
+            storage.append(scanner.nextLine());
+            System.out.println(storage);
+        }
+    }
+
+    private boolean isLineValid(List<String> line) {
+        if (line == null) {
+            return false;
+        }
+        if (line ==)
+    }
 }
---
+++ src/main/java/pl/coderstrust/numbersProcessor/Processor.java
@@ -0,0 +1,20 @@
+package pl.coderstrust.numbersProcessor;
+
+public class Processor {
+    private NumbersProcessor numbersProcessor;
+    private FileProcessor fileProcessor;
+
+    public Processor(NumbersProcessor numbersProcessor, FileProcessor fileProcessor) {
+        this.numbersProcessor = numbersProcessor;
+        this.fileProcessor = fileProcessor;
+    }
+
+//    public void process(String fileName, String resultFileName) {
+//        List<String> linesFromFile = fileProcessor.readLinesFromFile(fileName);
+//        List<String> resultLines = new ArrayList<>();
+//        for (String line : linesFromFile) {
+//            resultLines.add(numbersProcessor.processLine(line));
+//        }
+//        fileProcessor.writeLinesToFile(resultLines, resultFileName);
+//    }
+}
