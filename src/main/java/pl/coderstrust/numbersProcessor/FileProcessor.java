1ed13e15dd66e33f63f810485af22d1b273c19b7
Jagoda <jagodaciniak@gmail.com>
2018-09-05 23:45:57 +0200

    in process

--- src/main/java/pl/coderstrust/numbersProcessor/FileProcessor.java
+++ src/main/java/pl/coderstrust/numbersProcessor/FileProcessor.java
@@ -1,17 +1,15 @@
 package pl.coderstrust.numbersProcessor;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class FileProcessor {
 
-    public List<String> readLinesFromFile(String filepath) {
+    public List<String> readLinesFromFile(String filepath) throws FileNotFoundException {
         List<String> lines = new ArrayList<>();
-        Scanner scanner = new Scanner(filepath);
+        Scanner scanner = new Scanner(new File(filepath));
         while (scanner.hasNextLine()) {
             lines.add(scanner.nextLine());
         }
@@ -19,15 +17,11 @@ public class FileProcessor {
         return lines;
     }
 
-    public static void writeLinesToFile(List<String> lines, String fileName) {
-        FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(fileName);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void writeLinesToFile(List<String> lines, String fileName) throws IOException {
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName));
+        for (String line : lines) {
+            printWriter.println(line);
         }
-        PrintWriter printWriter = new PrintWriter(fileWriter);
-        printWriter.print(lines);
         printWriter.close();
     }
 }
--- src/main/java/pl/coderstrust/numbersProcessor/Main.java
+++ src/main/java/pl/coderstrust/numbersProcessor/Main.java
@@ -1,7 +1,12 @@
 package pl.coderstrust.numbersProcessor;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileProcessor fileprocessor = new FileProcessor();
+        NumbersProcessor numberprocessor = new NumbersProcessor();
+        Processor procesor = new Processor(numberprocessor, fileprocessor);
+        procesor.process("src/main/resources/1000", "src/main/resources/1000out");
     }
 }
