5974e05fa1a55edf58cf205e4e7de4725a54837f
Jagoda <jagodaciniak@gmail.com>
2018-09-12 19:17:13 +0200

    in process

--- src/test/java/pl/coderstrust/numbersProcessor/ProcessorTest.java
+++ src/test/java/pl/coderstrust/numbersProcessor/ProcessorTest.java
@@ -9,7 +9,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProcessorTest {
@@ -27,7 +28,7 @@ public class ProcessorTest {
         // given
         when(fileProcessor.readLinesFromFile("src/main/resources/1000")).thenReturn(Arrays.asList("1 2 3", "4 5 6"));
         when(numbersProcessor.processLine("1 2 3")).thenReturn("1+2+3=6");
-        doThrow(new Exception()).when(fileProcessor.writeLinesToFile(Arrays.asList("1+2+3=6", "4+5=9"), "1000out"));
+//        doThrow(new Exception()).when(fileProcessor.writeLinesToFile(Arrays.asList("1+2+3=6", "4+5=9"), "1000out"));
         // when
         processor.process("src/main/resources/1000", "src/main/resources/1000out");
 
