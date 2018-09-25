22f93a03e87e04ab9826d497108528ae1c7d751f
Jagoda <jagodaciniak@gmail.com>
2018-09-25 20:43:27 +0200

    in process

--- src/test/java/pl/coderstrust/sort/SortingTestBase.java
+++ src/test/java/pl/coderstrust/sort/SortingTestBase.java
@@ -9,7 +9,7 @@ public abstract class SortingTestBase {
     public abstract SortingMethod getSortingMethod();
 
     @Test
-    public void shouldSortSimpleArray() {
+    public void testForSortSimpleArray() {
         // given
         int[] given = new int[]{5, 4, 3, 2, 1};
         int[] expected = new int[]{1, 2, 3, 4, 5};
@@ -25,4 +25,23 @@ public abstract class SortingTestBase {
         // then
         assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void testForEmptyArray() {
+        // given
+        int[] given = new int[]{};
+        int[] expected = new int[]{};
+
+        // when
+        long startTime = System.currentTimeMillis();
+        SortingMethod test = getSortingMethod();
+        int[] result = test.sort(given);
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Time: " + (endTime - startTime));
+
+        // then
+        assertArrayEquals(expected, result);
+    }
+    
 }
