bcf415d3a08ca5055f7e9d3e56d8505dfb840bac
Jagoda <jagodaciniak@gmail.com>
2018-10-01 19:16:23 +0200

    adding

--- src/test/java/pl/coderstrust/sort/BubbleSortTest.java
+++ src/test/java/pl/coderstrust/sort/BubbleSortTest.java
@@ -1,6 +1,13 @@
 package pl.coderstrust.sort;
 
+import org.junit.BeforeClass;
+
 public class BubbleSortTest extends SortingTestBase {
+
+    @BeforeClass
+    public static void before() {
+        System.out.println(BubbleSortTest.class.getSimpleName() + " tested");
+    }
 
     @Override
     public SortingMethod getSortingMethod() {
--- src/test/java/pl/coderstrust/sort/CollectionsSortTest.java
+++ src/test/java/pl/coderstrust/sort/CollectionsSortTest.java
@@ -1,6 +1,13 @@
 package pl.coderstrust.sort;
 
+import org.junit.BeforeClass;
+
 public class CollectionsSortTest extends SortingTestBase {
+
+    @BeforeClass
+    public static void before() {
+        System.out.println(CollectionsSortTest.class.getSimpleName() + " tested");
+    }
 
     @Override
     public SortingMethod getSortingMethod() {
